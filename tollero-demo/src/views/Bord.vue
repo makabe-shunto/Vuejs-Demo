b44d53b07112389aa51e0b302fc919bca1d0fd49
makabe-shunto <makabe.shunto@sogo-net.co.jp>
2021-07-20 13:00:29 +0900

    コンポーネント追加

---
+++ tollero-demo/src/views/Bord.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="bord">
+        <h2 class="bord__ttl">カード一覧</h2>
+        
+    </div>
+</template>
+
+<style lang="scss" scoped>
+  .bord {
+    padding-top: 2rem;
+    background-image: url("../assets/bg.jpg");
+    background-size: cover;
+    background-position: center;
+    &__wrap {
+      overflow-x: scroll;
+      height: calc(100vh - 11rem);
+      padding: 0 2rem;
+      margin-top: 2rem;
+    }
+    &__cards {
+      display: inline-flex;
+      align-items: flex-start;
+    }
+    &__ttl {
+      margin-left: 2rem;
+      color: #ffffff;
+    }
+  }
+</style>
